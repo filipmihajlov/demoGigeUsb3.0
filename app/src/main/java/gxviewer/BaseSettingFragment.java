c86566255f4b06eadc1a1d5a9d35ebae9369a7d7
Filip Mihajlov <47906006+filipmihajlov@users.noreply.github.com>
2023-05-30 09:34:47 +0200

    initial commit of project

---
+++ app/src/main/java/gxviewer/BaseSettingFragment.java
@@ -0,0 +1,35 @@
+package gxviewer;
+
+import androidx.fragment.app.Fragment;
+import galaxy.Device;
+
+public abstract class BaseSettingFragment extends Fragment {
+
+    protected Device m_device;                    ///< keep camera device obj
+    protected ThreadShareObj m_threadShareObj;    ///< shared obj among threads
+
+    public BaseSettingFragment() {
+        super();
+    }
+
+    abstract void initUI();
+    /**
+     * brief    set m_device and m_threadShareObj
+     * param dev[in]               device obj
+     * param threadShareObj[in]    ThreadShareObj
+     */
+    public void setDeviceAndThreadShareObj(Device dev, ThreadShareObj threadShareObj) {
+        m_device = dev;
+        m_threadShareObj = threadShareObj;
+        initUI();
+    }
+
+    /**
+     * brief    when hide fragment, do something to clear member obj in fragment
+     *          e.g. Timer, Thread...
+     */
+    public void clearFragment()
+    {
+
+    }
+}
